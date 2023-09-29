1d640c27e3ca15710d7d63f8fcdb6200c09f8e8d
ffranhernandez1 <franciscohernandezv03@gmail.com>
2023-09-29 13:32:14 -0300

    tercera preEntrega

--- src/app.js
+++ src/app.js
@@ -21,23 +21,21 @@ import { PRODUCTS_DAO } from "./dao/index.js"
 import { ChatRepository } from "./dao/repository/chat.repository.js"
 import { MESSAGES_DAO } from "./dao/index.js"
 import { PRODUCTS_MODEL } from "./dao/mongo/models/products.js"
-//Configuración del dotenv
+
 configuration()
-//Inicializar express
+
 const app = express()
-//Guardar el puerto
+
 const PORT = process.env.PORT
-//Guardar la direccion de la base de Mongo
+
 const MONGO_URL = process.env.URL_MONGOOSE
-//Conectar con mongo
+
 mongoose.connect(MONGO_URL)
-//Modo de trabajo
+
 const ENVIRONMENT = process.env.ENVIRONMENT 
 
-//Cookie
 app.use(cookieParser("C0D3RS3CR3T"))
- 
-//Sesión con mongo
+
 app.use(session({ 
     store : MongoStore.create({
         mongoUrl: MONGO_URL,
@@ -52,32 +50,25 @@ app.use(session({
     saveUninitialized: false
 }))
 
-//Passport
 intializePassport()
 app.use(passport.initialize())
 app.use(passport.session()) 
 
-//Configuración del express
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
-//Configuración del handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, "./views"));
 
-
-//Uso de la carpeta public para ver el contenido / comunicación cliente servidor
 app.use(express.static("../public"))
 
-//Rutas
 app.use("/products",productsRouter)
 app.use("/carts",cartsRouter)
 app.use("/views",authToken,viewsRouter) 
 app.use("/chat",authToken,chatRouter)
 app.use("/",sessionRouter)
 
-//Inicializar el servidor con socket
 const server = app.listen(PORT,()=>{
     console.log("Escuchando desde el puerto " + PORT + " en modo " + ENVIRONMENT) 
 })
@@ -86,60 +77,48 @@ server.on("error",(err)=>{
     console.log(err)
 })
 
-//Io server
 const ioServer = new Server(server)
 
 const productsService = new ProductsRepository(PRODUCTS_DAO)
 const chatService = new ChatRepository(MESSAGES_DAO)
 
-//Conectarse
 ioServer.on("connection", async (socket) => {
     console.log("Nueva conexión establecida"); 
 
-    //Desconectarse
     socket.on("disconnect",()=>{
         console.log("Usuario desconectado")
     })
     
-      //Se suma un nuevo producto en realTime
       socket.on("new-product", async (data) => {
+
       const newProduct = await productsService.saveProduct(data) 
-      //Se muestran los productos realTime
       const productos = process.env.PORT === "8080" ? await PRODUCTS_MODEL.find({}).lean({}) : await productsService.getProducts()
       socket.emit("update-products", productos)
     });
-
-    //Se borra un producto en realTime 
+ 
     socket.on("delete-product",async(data)=>{  
         let id = data;
         let result = await productsService.deleteProduct(id);
         console.log("Producto eliminado", result);
-        //Se muestran los productos realTime
+
         const productos = process.env.PORT === "8080" ? await PRODUCTS_MODEL.find({}).lean({}) : await productsService.getProducts()
         socket.emit("update-products", productos)
     })
 
-    //Se muestran los productos realTime
     const productos = process.env.PORT === "8080" ? await PRODUCTS_MODEL.find({}).lean({}) : await productsService.getProducts()
     socket.emit("update-products", productos)
-    
-
-     /****/
-
-    //Crear mensaje
+ 
     socket.on("guardar-mensaje",async(data)=>{
        await chatService.createMessage(data)
        const mensajes = await chatService.getMessages()
        socket.emit("enviar-mensajes",mensajes)
     })
 
-    //Mostar mensajes
     const mensajes = await chatService.getMessages()
     socket.emit("enviar-mensajes",mensajes)
 
-    //Recibir cantidad de mensajes
     socket.on("Nuevos-mensajes",async(data)=>{
-        //Mostar mensajes
+
         const mensajes = await chatService.getMessages()
         socket.emit("enviar-mensajes",mensajes)
     })
--- src/dao/mongo/models/products.js
+++ src/dao/mongo/models/products.js
@@ -38,12 +38,11 @@ const productsSchema = new mongoose.Schema({
     },
     quantity :{
         type: Number,
-        require: true
+        require: true,
+        default: 1
     }
 })
 
 productsSchema.plugin(mongoosePaginate)
 
-const ProductsModel = mongoose.model(productsCollection,productsSchema)
-
-export default ProductsModel
+export const PRODUCTS_MODEL = mongoose.model(productsCollection,productsSchema)
